5cd6a95e55b1bfd16b2fb5c4cb094fd5b0f91d78
Avzgui <contact.avzgui@gmail.com>
2021-04-25 12:15:53 +0200

    posage echelles

--- Scripts/Events.gd
+++ Scripts/Events.gd
@@ -4,3 +4,6 @@ extends Node
 # To emit signals from a node, call Events.emit_signal("[action_in_past]_signal")
 # To receive signals in a node, call connect(Events, "[action_in_past]_signal", node, "_on_[slot name]")
 
+signal place_ladder_signal
+signal ladder_placed_signal
+signal ladder_not_placed_signal
